a64eb2008432ce18b23c229c5a34b5427e4cd121
Alexander <asensei047@gmail.com>
2018-04-01 17:44:49 +0300

    Initial Commit

---
+++ The Filterer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  The Filterer
+//
+//  Created by Alexander on 01.04.2018.
+//  Copyright © 2018 Alexander. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ The Filterer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  The Filterer
+//
+//  Created by Alexander on 01.04.2018.
+//  Copyright © 2018 Alexander. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
